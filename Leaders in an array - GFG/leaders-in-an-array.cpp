9851e488b7ab948d9150224e7a67f98c12985741
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2023-08-18 11:50:18 +0530

    Added solution - LeetHub

--- Leaders in an array - GFG/leaders-in-an-array.cpp
+++ Leaders in an array - GFG/leaders-in-an-array.cpp
@@ -13,17 +13,16 @@ class Solution{
     public:
     vector<int> leaders(int a[], int n){
         // Code here
-        int maxi[n]={0};
-        vector<int> ret;
-        int maxa=INT_MIN;
+        int maxi=INT_MIN;
+        vector<int> ans;
+        int l[n]={0};
         for(int i=n-1;i>=0;i--){
-            maxa=max(maxa,a[i]);
-            maxi[i]=maxa;
+            l[i]=maxi=max(maxi,a[i]);
         }
         for(int i=0;i<n;i++){
-            if(a[i]>=maxi[i]) ret.push_back(a[i]);
+            if(a[i]>=l[i]) ans.push_back(a[i]);
         }
-        return ret;
+        return ans;
     }
 };
 
