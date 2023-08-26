ce9973a4ec25c88c1a6e9e304a3c5086e20d31ff
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2023-08-27 00:45:00 +0530

    Added solution - LeetHub

--- Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
+++ Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
@@ -14,17 +14,22 @@ class Solution
     public:
     //Function to find length of shortest common supersequence of two strings.
     int lcs(string X, string Y, int m, int n,vector<vector<int>> &dp){
-        if(m<0 || n<0) return 0;
+        if(m==0 || n==0) return 0;
         
         if(dp[m][n]!=-1) return dp[m][n];
-        if(X[m]==Y[n]) return dp[m][n]=1+lcs(X,Y,m-1,n-1,dp);
+        if(X[m-1]==Y[n-1]) return dp[m][n]=1+lcs(X,Y,m-1,n-1,dp);
         return dp[m][n]= max(lcs(X,Y,m-1,n,dp),lcs(X,Y,m,n-1,dp));
     }
     int shortestCommonSupersequence(string X, string Y, int m, int n)
     {
         //code here
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return m+n-lcs(X,Y,m-1,n-1,dp);
+        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
+        // for(int i=1;i<m;i++){
+        //     for(int j=1;j<n;j++){
+                
+        //     }
+        // }
+        return m+n-lcs(X,Y,m,n,dp);
     }
 };
 
