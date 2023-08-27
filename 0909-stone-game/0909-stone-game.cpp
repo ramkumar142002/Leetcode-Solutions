51bbb8c48681e1e6d0bbee877858e7dd2799b44f
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2023-08-27 13:42:15 +0530

    Time: 0 ms (100%), Space: 7.5 MB (64.57%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,31 +1,38 @@
+// class Solution {
+// public:
+//     bool stoneGame(vector<int>& piles) {
+//         int i=0;
+//         int alice=0,bob=0;
+//         while(piles.size()>0){
+//             if(i%2==0){
+//                 if(piles[0]>piles[piles.size()-1]){
+//                     alice+=piles[0];
+//                     piles.erase(piles.begin());
+//                 }
+//                 else{
+//                     alice+=piles[piles.size()-1];
+//                     piles.pop_back();
+//                 }
+//             }
+//             else{
+//                 if(piles[0]>piles[piles.size()-1]){
+//                     bob+=piles[0];
+//                     piles.erase(piles.begin());
+//                 }
+//                 else{
+//                     bob+=piles[piles.size()-1];
+//                     piles.pop_back();
+//                 }
+//             }
+//         }
+//         if(alice>bob) return true;
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int i=0;
-        int alice=0,bob=0;
-        while(piles.size()>0){
-            if(i%2==0){
-                if(piles[0]>piles[piles.size()-1]){
-                    alice+=piles[0];
-                    piles.erase(piles.begin());
-                }
-                else{
-                    alice+=piles[piles.size()-1];
-                    piles.pop_back();
-                }
-            }
-            else{
-                if(piles[0]>piles[piles.size()-1]){
-                    bob+=piles[0];
-                    piles.erase(piles.begin());
-                }
-                else{
-                    bob+=piles[piles.size()-1];
-                    piles.pop_back();
-                }
-            }
-        }
-        if(alice>bob) return true;
-        return false;
+       return true;
     }
 };
