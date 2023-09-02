f8c19b93b007c65f382a1e7650fdc6f03e7eaf1a
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2023-09-02 16:33:14 +0530

    Time: 4 ms (77.16%) | Memory: 6.5 MB (41.27%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string a="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='i') reverse(a.begin(),a.end());
+            else a+=s[i];
+        }
+        return a;
+    }
+};
