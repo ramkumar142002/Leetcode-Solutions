3cd2a67191e4238c89a14aa30dc50e87cc3de211
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2023-09-07 22:41:45 +0530

    Time: 325 ms (49.59%) | Memory: 10.8 MB (39.24%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -5,26 +5,30 @@ public:
 
         if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
         int len=0+fun(nums,ind+1,prev,dp);
-        if(prev==-1 || nums[ind]>nums[prev+1]){
+        if(prev==-1 || nums[ind]>nums[prev]){
             len=max(len,1+fun(nums,ind+1,ind,dp));
         }
         return dp[ind][prev+1]=len;
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(nums.size()+1,vector<int> (nums.size()+1,0));
+        // vector<vector<int>> dp(nums.size()+1,vector<int> (nums.size()+1,0));
+
+        vector<int> next(n+1,0);
+        vector<int> cur(n+1,0);
         // return fun(nums,0,-1,dp);
 
         for(int ind=n-1;ind>=0;ind--){
             for(int prev=ind-1;prev>=-1;prev--){
 
-                int len=0+dp[ind+1][prev+1];
+                int len=0+next[prev+1];
                 if(prev==-1 || nums[ind]>nums[prev]){
-                    len=max(len,1+dp[ind+1][ind+1]);
+                    len=max(len,1+next[ind+1]);
                 }
-                dp[ind][prev+1]=len;
+                cur[prev+1]=len;
             }
+            next=cur;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
