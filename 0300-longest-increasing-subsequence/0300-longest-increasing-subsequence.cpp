9d483202db5c105389604514cd5d07dc9258a570
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2023-09-07 22:41:40 +0530

    Time: 493 ms (37.32%), Space: 292.2 MB (26.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,13 +5,26 @@ public:
 
         if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
         int len=0+fun(nums,ind+1,prev,dp);
-        if(prev==-1 || nums[ind]>nums[prev]){
+        if(prev==-1 || nums[ind]>nums[prev+1]){
             len=max(len,1+fun(nums,ind+1,ind,dp));
         }
         return dp[ind][prev+1]=len;
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int> (nums.size()+1,-1));
-        return fun(nums,0,-1,dp);
+        int n=nums.size();
+        vector<vector<int>> dp(nums.size()+1,vector<int> (nums.size()+1,0));
+        // return fun(nums,0,-1,dp);
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev=ind-1;prev>=-1;prev--){
+
+                int len=0+dp[ind+1][prev+1];
+                if(prev==-1 || nums[ind]>nums[prev]){
+                    len=max(len,1+dp[ind+1][ind+1]);
+                }
+                dp[ind][prev+1]=len;
+            }
+        }
+        return dp[0][0];
     }
 };
