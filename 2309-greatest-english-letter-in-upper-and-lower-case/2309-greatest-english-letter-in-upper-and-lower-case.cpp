daa2169282cf60c41482767e73b5b644e1c7a1aa
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2023-10-03 22:15:50 +0530

    Time: 0 ms (100.00%), Space: 7 MB (42.21%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     string greatestLetter(string s) {
-        vector<int> u(26,0),l(26,0);
-        for(int i=0;i<s.size();i++){
-            if(isupper(s[i])){
-                u[s[i]-'A']++;
-            }
-            else{
-                l[s[i]-'a']++;
-            }
-        }
-        
-        for(int i=25;i>=0;i--){
-            if(u[i]>0 && l[i]>0){
-                string ans="";
-                 ans+=(i+'A') ;
-                return ans;
-            }
-        }
-        return "";
-    }
+    int cnt[128] = {};
+    for (auto ch : s)
+        ++cnt[ch];
+    for (auto ch = 'Z'; ch >= 'A'; --ch)
+        if (cnt[ch] && cnt[ch + 'a' - 'A'])
+            return string(1, ch);
+    return "";
+}
 };
