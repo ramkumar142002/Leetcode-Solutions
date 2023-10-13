cc2ba8642783a935c4e1a644a4cadec1f65f368e
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2023-10-13 11:14:43 +0530

    Added solution - LeetHub

---
+++ Floor in BST - GFG/floor-in-bst.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// Function to search a node in BST.
+class Solution{
+
+public:
+    vector<int> ans;
+    void fun(Node* root){
+        if(root==NULL) return ;
+        
+        fun(root->left);
+        ans.push_back(root->data);
+        fun(root->right);
+    }
+    int floor(Node* root, int x) {
+        // Code here
+        fun(root);
+        sort(ans.begin(),ans.end(),greater<int>());
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]<=x) return ans[i];
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+        Solution obj;
+        cout << obj.floor(root, s) << "\n";
+    }
+}
+
+// } Driver Code Ends
