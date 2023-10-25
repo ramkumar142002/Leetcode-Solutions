a883294a27feb2ce2cf1fe2d1f9d648400cd6007
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2023-10-25 11:25:19 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -10,8 +10,8 @@ using namespace std;
 class Solution{
 public:
     int fun(int ind,int w,int val[],int wt[],vector<vector<int>> &dp){
-        if(ind == 0){
-            if(wt[ind]<=w) return val[ind]+fun(ind,w-wt[ind],val,wt,dp);
+        if(ind == -1){
+            //if(w%wt[ind]==0) return val[ind]*(w/wt[ind]);
             return 0;
         }
         
