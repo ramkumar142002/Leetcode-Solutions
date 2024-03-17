51e31e4bfeded0f39ac674a1db5b09f091a12214
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2024-03-17 13:37:55 +0530

    Time: 1 ms (62.66%), Space: 42.7 MB (49.45%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,25 @@
 class Solution {
-public:
-    void fun(vector<int> &nums,int ind,vector<vector<int>> &ans,vector<int> &temp){
-        if(ind==nums.size()){
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(nums[ind]);
-        fun(nums,ind+1,ans,temp);
+    public List<List<Integer>> subsets(int[] nums) {
         
-        temp.pop_back();
-        fun(nums,ind+1,ans,temp);
         
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
         // vector<int> flag(nums.size(),0);
         fun(nums,0,ans,temp);
         return ans;
     }
-};
+        
+     public static void fun(int[] nums,int ind,List<List<Integer>> ans,List<Integer> temp){
+        if(ind==nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        temp.add(nums[ind]);
+        fun(nums,ind+1,ans,temp);
+
+        temp.remove(temp.size()-1);
+        fun(nums,ind+1,ans,temp);
+
+    }
+    
+}
