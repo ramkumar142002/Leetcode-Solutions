0906218e9321853c00ee7ba163456fd443489779
ramkumar142002 <95877257+ramkumar142002@users.noreply.github.com>
2024-07-26 11:16:30 +0530

    Time: 1 ms (24.06%), Space: 41.4 MB (71.66%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,9 +2,22 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
         for(int[] a : dp){
-            Arrays.fill(a,-1);
+            Arrays.fill(a,0);
         }
-        return fun(obstacleGrid,obstacleGrid.length-1,obstacleGrid[0].length-1,dp);
+        // return fun(obstacleGrid,obstacleGrid.length-1,obstacleGrid[0].length-1,dp);
+        for(int row = 0;row<obstacleGrid.length;row++){
+            for(int col = 0;col<obstacleGrid[0].length;col++){
+                if(obstacleGrid[row][col]==1) dp[row][col] = 0;
+                else if(row==0 && col==0) dp[row][col] = 1;
+                else{
+                    int left = 0,up = 0;
+                    if(col>0) left = dp[row][col-1];
+                    if(row>0) up = dp[row-1][col];
+                    dp[row][col] = left+up;
+                }
+            }
+        }
+        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
     }
     public int fun(int[][] obstacleGrid,int row,int col,int[][] dp){
         if(row >= 0 && col>=0 && obstacleGrid[row][col]==1) return 0;
